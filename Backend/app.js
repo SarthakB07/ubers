32d977bc7e603e62b4d92bbb0c14fee31a06a8b1
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-12-24 23:44:55 +0530

    Db connected

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,11 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
+
+connectToDb();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
