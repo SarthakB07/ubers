97b6698f0ba4ddd45b49a889b6c3afc69cec23c8
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-12-29 00:27:10 +0530

    user and captain frontend created

---
+++ Frontend/src/pages/UserLogout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const USerLogout = () => {
+  return (
+    <div>USerLogout</div>
+  )
+}
+
+export default USerLogout
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // iska matlab tailwind css index mei kaam karegi and src mei bhi
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
