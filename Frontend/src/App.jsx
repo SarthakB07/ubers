8c40e42df0216cf088778dcd11e5a072d1d7230f
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-12-29 23:50:42 +0530

    frontend and backend of user created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {Route,Routes} from 'react-router-dom'
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
-import Home from './pages/Home'
+import Start from './pages/Start'
 import CaptainHome from './pages/CaptainHome'
+import Home from './pages/Home'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
+
 
 const App = () => {
   return (
     <div>
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Start />} />
       <Route path='/captain-home' element={<CaptainHome />} />
       <Route path='/login' element={<UserLogin />} />
       <Route path='/signup' element={<UserSignup />} />
       <Route path='/captain-login' element={<CaptainLogin />} />
       <Route path='/captain-signup' element={<CaptainSignup />} />
+      <Route path='/home' element={
+          <UserProtectWrapper>
+            <Home />
+          </UserProtectWrapper>
+        } />
+        <Route path='/user/logout' element={<UserProtectWrapper>
+          <UserLogout />
+        </UserProtectWrapper>
+        } />
       </Routes>    
       </div>
   )
