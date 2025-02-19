dc5bbabf3b13c5943b5c8d319e252d5948500bfe
SarthakB07 <sarthak.bansal@spit.ac.in>
2025-02-19 15:28:34 +0530

    socket integrstion with real time fare

--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports.getAddressCoordinate = async (address) => {
-    const apiKey = process.env.GOOGLE_MAPS_API;
+    const apiKey = process.env.GOOGLE_MAPS_API ;
     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
 
     try {
--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
-const LocationSearchPanel = (props) => {
-    // sample array for location 
-    const locations = [
-        "A27, Near TPS School, Dr virender Singh, Jaipur",
-        "22C, Near Ganpati Plaza,Katawa Bhawan, Jaipur",
-        "20B, Near Genpact, Ricco Industrial Area, Jaipur",
-        "18A, Near Sharma's cafe, Jai Shree Periwal School, Jaipur",
-    ]
+
+const LocationSearchPanel = ({ suggestions, setVehiclePanel, setPanelOpen, setPickup, setDestination, activeField }) => {
+
+    const handleSuggestionClick = (suggestion) => {
+        if (activeField === 'pickup') {
+            setPickup(suggestion)
+        } else if (activeField === 'destination') {
+            setDestination(suggestion)
+        }
+        // setVehiclePanel(true)
+        // setPanelOpen(false)
+    }
+
     return (
         <div>
-            {/* this is just a sample data  */}
+            {/* Display fetched suggestions */}
             {
-                locations.map(function (elem, idx) {
-                    return <div key={idx} onClick={() => {
-                        props.setVehiclePanel(true)
-                        props.setPanelOpen(false)
-                    }} className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
+                suggestions.map((elem, idx) => (
+                    <div key={idx} onClick={() => handleSuggestionClick(elem)} className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
                         <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
                         <h4 className='font-medium'>{elem}</h4>
                     </div>
-                })
+                ))
             }
         </div>
     )
 }
+
 export default LocationSearchPanel
